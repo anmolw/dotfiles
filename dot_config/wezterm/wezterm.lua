5d97406dd267d36b5f7f019eeb6af8898c5033eb
Anmol Wadhwani <anmolsw@gmail.com>
2024-08-23 21:57:19 +0530

    Add initial wezterm bindings + platform overrides

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,17 +1,29 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+
+local bindings = require 'bindings'
 local wayland_cursor = require 'wayland_cursor'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.default_prog = { '/usr/bin/fish', '-l' }
+bindings.apply_to_config(config)
 
-wayland_cursor.apply_to_config(config)
+-- Linux-specific configuration
+if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
+  wayland_cursor.apply_to_config(config)
+  config.default_prog = { '/usr/bin/fish', '-l' }
+-- Windows-specific configuration
+elseif wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  config.default_prog = { 'pwsh' }
+end
 
-config.color_scheme = 'Tokyo Night'
-config.font = wezterm.font "Hack Nerd Font Mono"
+config.color_scheme = 'Catppuccin Mocha'
+config.font = wezterm.font 'Hack Nerd Font Mono'
+
+-- No ligatures
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
 config.initial_rows = 40
 config.initial_cols = 132
