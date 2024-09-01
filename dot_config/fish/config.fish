83b87857b7966a5f3e9098616f5f099ac59a0dd2
anmolw <4815989+anmolw@users.noreply.github.com>
2024-09-01 20:45:10 +0530

    mise integration

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -19,6 +19,10 @@ if status --is-interactive
   end
 end
 
+if command -q mise
+  mise activate fish | source
+end
+
 if set -q WSL_DISTRO_NAME
   if not set -q SSH_AUTH_SOCK
     SHELL="$(which fish)" wsl2-ssh-agent | source
@@ -44,6 +48,7 @@ end
 
 if command -q lsb_release
   set distrib $(lsb_release -si)
+  # Aliases for arch & derivatives
   if test "$distrib" = "arch"; or test "$distrib" = "cachyos"; or test "$distrib" = "endeavouros"
     alias fixpacman="sudo rm /var/lib/pacman/db.lck"
     # List amount of -git packages
@@ -66,6 +71,8 @@ end
 
 # Colorize grep output
 alias grep "grep --color=auto"
+alias fgrep "fgrep --color=auto"
+alias egrep "egrep --color=auto"
 
 # Exclude extraneous mounts from df
 alias dfx "df -h -x tmpfs -x overlay -x nfs -x smbfs -x cifs -x fuse.sshfs -x devtmpfs -x squashfs"
